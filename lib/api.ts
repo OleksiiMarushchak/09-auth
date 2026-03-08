6c82a5bb78473578192cdd1af824c046cd0316b5
Oleksii Marushchak <OleksiiMarushchak@outlook.com>
2026-03-08 23:09:39 +0000

    update api.ts

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,8 @@
 import axios, { AxiosError } from 'axios';
-import type { User } from '../types/user';
-import type { Note } from '../types/note';
+
 export type ApiError = AxiosError<{ error: string }>;
 
 export const nextServer = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     withCredentials: true, // дозволяє axios працювати з cookie
 });
